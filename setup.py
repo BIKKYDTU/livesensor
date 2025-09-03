033a1cc0ca35f833f01568ccc0a4745a66358700
bikky <kumarbikky8340@gmail.com>
2025-09-03 23:47:00 +0530

    Readme update

--- setup.py
+++ setup.py
@@ -1,50 +1,19 @@
-# from setuptools import find_packages ,setup 
-# from typing import List
+from  setuptools import find_packages, setup
+#from  typing import List 
 
 
+def get_requirements() ->list[str]:
+      reuirements_list = list[str] = []
 
-# def get_requirements()->List[str]:
+      return reuirements_list 
 
-#     reuirements_list : List[str] =[]
+setup (
+ name = 'sensor',
+ version = "0.0.1",
+ author = "prince",
+ author_email = "kumarbikky8340@gmail.com",
+ packages = find_packages(),
+ install_requires =  get_requirements(), #["pymongo"]
 
-#     return reuirements_list
-
-
-
-
-
-
-# setup (
-#     name = 'sensor',
-#     version="0.0.1",
-#     author="prince",
-#     author_email="princekatiyar986@gmail.com",
-#     packages = find_packages(),
-#     install_requires = get_requirements() , #["pymongo"]
-
-# )
-
-from setuptools import find_packages,setup
-from typing import List
-
-def get_requirements()->List[str]:
-    """
-
-
-    This function will return list of requirements
-    """
-    requirement_list:List[str] = []
-
-    """
-    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
-    """
-    return requirement_list
-
-setup(
-    name="sensor",
-    version="0.0.1",
-    author="ineuron",
-    author_email="princekatiyar986@gmail.com",
-    packages = find_packages(),
-    install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
+
